69d172b237db20089ecefcdb251e1897c8d78da1
Valerie Freeman <valerah7@gmail.com>
2024-05-30 10:04:20 -0500

    created the inventions component

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,5 @@
-import database from "./database.js";
+import { Inventions } from "./inventions.js"
 
-let inventionsHtml = "";
-for (let i in database.inventions){
-    const invention = database.inventions[i];
-    inventionsHtml += `<li><p>🧪 ${invention.name}</p><p class="description">${invention.description}</p></li>`;
-}
-const inventionContentBlock = document.querySelector("#invention-content");
-inventionContentBlock.innerHTML = inventionsHtml;
+const inventionContainer = document.getElementById("invention-content")
 
-
-const listings = database.experiments.map( experiment => {
-    const icon = experiment.succeeded ? "✔️" : "❌";
-    return `<li>${icon} ${experiment.name}</li>`;
-});
-const listingsHtml = listings.join('');
-const experimentContentBlock = document.querySelector("#experiment-content");
-experimentContentBlock.innerHTML = listingsHtml;
+inventionContainer.innerHTML = Inventions()
